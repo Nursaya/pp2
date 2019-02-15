c90e38f163cd006918e3745b75e58506f832db9f
Nursaya <47138319+Nursaya@users.noreply.github.com>
2019-02-15 22:25:47 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,48 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp12
+namespace ConsoleApp10
 {
     class Program
-    {   /// <summary>
-        /// defining prime numbers
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        public static bool IsPrime(int x)
+    {/// <summary>
+     /// defining whether it is palindrome or not
+     /// </summary>
+     /// <param name="s"></param>
+     /// <param name="k"></param>
+     /// <returns></returns>
+        public static bool IsPalindrome(string s, int k)
         {
-            if (x <= 1) // by definition of prime numbers: they have to be more than 1
+            for (int i = 0; i < k / 2; i++)
             {
-                return false;
-            }
-            for (int i = 2; i <= Math.Sqrt(x); ++i)
-            {
-                if (x % i == 0) //by definition prime numbers can be divided by 1 and themselves only
-                {
+                if (s[i] != s[k - i - 1])
                     return false;
-                }
             }
-            return true; // if two conditions above are not hold, it is prime number
+            return true;
         }
 
         static void Main(string[] args)
         {
-            string numbers = System.IO.File.ReadAllText("C:/Users/Нурсая/Desktop/pp2/week 2/task 1/input.txt"); // reading the numbers from the file
-            int[] a = new int[numbers.Length / 2]; // divided by 2 because the line contains spaces
-            a = numbers.Split(' ').Select(int.Parse).ToArray();
-            List<int> list = new List<int>(); // creating list to store prime numbers
-            for (int i = 0; i < a.Length; ++i)
+            string text = System.IO.File.ReadAllText(@"C:\Users\Нурсая\Desktop\nurs.txt"); // reading the numbers from the file
+            int k = text.Length;
+            if (IsPalindrome(text, k) == true)
             {
-                if (IsPrime(a[i]) == true)
-                {
-                    list.Add(a[i]); // adding prime numbers to the list
+                Console.WriteLine("Yes");
 
-                }
             }
-
-            var result = String.Join(" ", list.ToArray()); // converting the list into string
-            System.IO.File.WriteAllText("C:/Users/Нурсая/Desktop/pp2/week 2/task 1/output.txt", result); // writing prime numbers to the new file
-
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
